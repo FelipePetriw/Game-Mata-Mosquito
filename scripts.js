4174d1ceecf3e8d5268a92a9695f501836e7897e
Felipe Petriw <felipe.petriw.ads@gmail.com>
2023-03-10 17:29:58 -0300

    Seleção de nivel
    Aplicação de script para selecionar o nível que deseja jogar o game.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,19 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'facil'){
+    criaMosquitoTempo = 1500
+} else if(nivel === 'normal') {
+    criaMosquitoTempo = 1000
+} else if (nivel === 'dificil') {
+    criaMosquitoTempo = 750
+}
+
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
